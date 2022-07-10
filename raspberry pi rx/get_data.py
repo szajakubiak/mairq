cfcde8471bccaf378fdf76abc2d1af01c31dc4e6
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2022-07-10 09:36:58 +0200

    Read LoRa data using Raspberry Pi

---
+++ raspberry pi rx/get_data.py
@@ -0,0 +1,35 @@
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import adafruit_rfm9x
+
+CS = DigitalInOut(board.D7)
+RESET = DigitalInOut(board.D22)
+spi = busio.SPI(board.D11, MOSI = board.D10, MISO = board.D9)
+
+# Output file
+output = "mairq_data.txt"
+
+try:
+   rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 433.8)
+except RuntimeError as error:
+   print("RFM9x error: ", error)
+
+rfm9x.tx_power = 12
+
+while True:
+   packet = None
+   packet = rfm9x.receive()
+
+   if packet is not None:
+      try:
+        packet = packet.split(b"\x00")[0]
+        packet_text = str(packet, "utf-8")
+
+        with open(output, "a") as file:
+          file.write(packet_text + "\n")
+
+        response = bytes("OK\0", "utf-8")
+        rfm9x.send(response)
+      except:
+         pass
