b92677c666cfee6ba1b8b2ff848a7b8c784f9761
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2022-09-01 13:14:30 +0200

    Add comments to code

--- raspberry pi rx/get_data.py
+++ raspberry pi rx/get_data.py
@@ -3,8 +3,11 @@ import busio
 from digitalio import DigitalInOut
 import adafruit_rfm9x
 
+# LoRa module pin definitions
 CS = DigitalInOut(board.D7)
 RESET = DigitalInOut(board.D22)
+
+# Init SPI interface to communicate with LoRa module
 spi = busio.SPI(board.D11, MOSI = board.D10, MISO = board.D9)
 
 # Output file
@@ -15,20 +18,26 @@ try:
 except RuntimeError as error:
    print("RFM9x error: ", error)
 
+# Set transmision power for LoRa module
 rfm9x.tx_power = 12
 
+# Main loop
 while True:
    packet = None
    packet = rfm9x.receive()
 
+   # If there is data in packet
    if packet is not None:
       try:
         packet = packet.split(b"\x00")[0]
+        # Decode data
         packet_text = str(packet, "utf-8")
 
+        # Write data to file
         with open(output, "a") as file:
           file.write(packet_text + "\n")
-
+         
+        # Send confirmation that message was received 
         response = bytes("OK\0", "utf-8")
         rfm9x.send(response)
       except:
