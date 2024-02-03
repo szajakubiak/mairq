79ee623171efe7ab870aa565d3b19c128f49b99d
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2024-02-03 21:58:23 +0100

    Code formatting

--- raspberry pi rx/get_data.py
+++ raspberry pi rx/get_data.py
@@ -8,37 +8,37 @@ CS = DigitalInOut(board.D7)
 RESET = DigitalInOut(board.D22)
 
 # Init SPI interface to communicate with LoRa module
-spi = busio.SPI(board.D11, MOSI = board.D10, MISO = board.D9)
+spi = busio.SPI(board.D11, MOSI=board.D10, MISO=board.D9)
 
 # Output file
 output = "mairq_data.txt"
 
 try:
-   rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 433.8)
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 433.8)
 except RuntimeError as error:
-   print("RFM9x error: ", error)
+    print("RFM9x error: ", error)
 
 # Set transmision power for LoRa module
 rfm9x.tx_power = 12
 
 # Main loop
 while True:
-   packet = None
-   packet = rfm9x.receive()
+    packet = None
+    packet = rfm9x.receive()
 
-   # If there is data in packet
-   if packet is not None:
-      try:
-         packet = packet.split(b"\x00")[0]
-         # Decode data
-         packet_text = str(packet, "utf-8")
+    # If there is data in packet
+    if packet is not None:
+        try:
+            packet = packet.split(b"\x00")[0]
+            # Decode data
+            packet_text = str(packet, "utf-8")
 
-         # Write data to file
-         with open(output, "a") as file:
-            file.write(packet_text + "\n")
-         
-         # Send confirmation that message was received 
-         response = bytes("OK\0", "utf-8")
-         rfm9x.send(response)
-      except:
-         pass
+            # Write data to file
+            with open(output, "a") as file:
+                file.write(packet_text + "\n")
+
+            # Send confirmation that message was received
+            response = bytes("OK\0", "utf-8")
+            rfm9x.send(response)
+        except:
+            pass
